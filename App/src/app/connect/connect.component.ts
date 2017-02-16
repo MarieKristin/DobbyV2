9736029491843d208d2220df73e393b893b82c5e
MarieKristin <die.gwyn@googlemail.com>
2017-02-16 14:04:50 +0100

    Ablaufanpassung für Manuelle Steuerung

--- App/src/app/connect/connect.component.ts
+++ App/src/app/connect/connect.component.ts
@@ -15,23 +15,31 @@ export class ConnectComponent {
 
   private helpArr: HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('hidden');
   private helpEl: HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('btn-prim');
+  private loader: HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('loader');
 
   // TODO: In einen Angular 2 Service schieben
   constructor() {  }
 
   public connect() {
     var i = 0;
+
+    this.helpEl[0].style.display = "none";
+    this.loader[0].style.visibility = "visible";
+
     this._ws = new WebSocket('ws://192.168.0.1:8080');
     //this._ws = new WebSocket('ws://192.168.178.50:8080');
 
-    this._ws.onmessage = event => {
-      this.history.push('[SERVER] ' + event.data);
-    };
+    this._ws.onopen = event => {
+      this.loader[0].style.visibility = "hidden";
 
-    for (i=0; i< this.helpArr.length; i++) {
-      this.helpArr[i].style.display = "block";
+      this._ws.onmessage = event => {
+        this.history.push('[SERVER] ' + event.data);
+      };
+
+      for (i=0; i< 3; i++) {
+        this.helpArr[i].style.display = "block";
+      }
     }
-    this.helpEl[0].style.display = "none";
   }
 
   public auto() {
@@ -49,6 +57,7 @@ export class ConnectComponent {
   }
 
   public man() {
+    var i = 0;
     //if (!this.command) {
     //  return;
     //}
@@ -60,5 +69,22 @@ export class ConnectComponent {
     this.history.push('[CLIENT] ' + 'manual');
 
     //this.command = '';
+    for (i=0; i< 3; i++) {
+      this.helpArr[i].style.display = "none";
+    }
+    for (i=3; i< this.helpArr.length; i++) {
+      this.helpArr[i].style.display = "block";
+    }
+  }
+
+  public back() {
+    var i = 0;
+
+    for (i=3; i< this.helpArr.length; i++) {
+      this.helpArr[i].style.display = "none";
+    }
+    for (i=0; i< 3; i++) {
+      this.helpArr[i].style.display = "block";
+    }
   }
 }
