7474545fb3a1436506aae8493ccc498d28fefa17
danielstumpf <danielstumpf87@gmx.de>
2017-03-07 18:34:20 +0100

    extracted Frame class2

--- Raspberry/home/pi/Dobby/lin.h
+++ Raspberry/home/pi/Dobby/lin.h
@@ -1,39 +1,26 @@
-#ifndef LIN_H
-#define LIN_H
+#pragma once
+enum FrameType
+{
+	init, activation, message	//0, 1, 2
+};
 
 class Lin
 {
 public:
 	Lin();
 	~Lin();
-	extern void startMotors(int directionLeft, int velocityLeft, int directionRight, int velocityRight);
-	extern void stopMotors();
-	void initialiseSend();
-	int linInitialise();
+	void setInitFrame(int data0, int data1, int data2, int data3, int data4,
+		int data5, int data6, int data7, int data8);
+	void setActivationFrame(int data0, int data1, int data2, int data3, int data4,
+		int data5, int data6, int data7, int data8);
+	void setMessageFrame(int data0, int data1, int data2, int data3, int data4,
+		int data5, int data6, int data7, int data8);
+	void getFrame();
+
 private:
-	int BAUDRATE;
-	int handle;
-	enum {
-		message = 1, activation = 2, init = 3
-	};
-	struct Frame{
-		int id;
-		int frameContent[10];
-	};
+	int BAUDRATE = 19200;
+	int handle = -1;
 	Frame initFrame;
-	Frame messageFrame;
 	Frame activationFrame;
-
-
-	int setFrame(int id, int data0, int data1, int data2, int data3, int data4,
-			int data5, int data6, int data7, int data8);
-	int getChecksum(int frame[]);
-	void sendMessageFrame();
-	void sendActivationFrame();
-	void sendInitFrame();
-	void sendBreak();
-	void sendWakeUp();
-	void sendSyncByte();
-	void setInitialContents();
+	Frame messageFrame;
 };
-#endif /* LIN_H */
