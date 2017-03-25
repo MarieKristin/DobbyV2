573eef4a4dcba1ba3b9da3ee697e6c5920dec7e0
MarieKristin <die.gwyn@googlemail.com>
2017-03-25 15:30:42 +0100

    Menüpunkt "Aktiv"-Anzeige eingebaut

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements AfterViewInit{
   private menu:HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('menu');
   private buttonMenu:HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('menuButton');
+  private menuElements:HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('linkEl');
+
+  public ngAfterViewInit() {
+    var firstElement = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('linkEl');
+    firstElement[0].classList.toggle('menuActive');
+  }
 
   public menuClicked() {
     var menu = this.menu[0];
+    menu.classList.toggle('open');
+  }
+
+  public menuLinkClicked(linkElement) {
+    var menu = this.menu[0];
+    var el = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('menuActive');
+    el[0].classList.toggle('menuActive');
+    this.menuElements[linkElement].classList.toggle('menuActive');
     menu.classList.toggle('open');
   }
 }
