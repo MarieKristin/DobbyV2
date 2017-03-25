26ad01b24c4967904e19b942826228c2f949af0e
MarieKristin <die.gwyn@googlemail.com>
2017-03-25 14:39:32 +0100

    Schiebemenü eingebaut

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent{
+  private menu:HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('menu');
+  private buttonMenu:HTMLCollectionOf<HTMLElement> = <HTMLCollectionOf<HTMLElement>>document.getElementsByClassName('menuButton');
+
+  public menuClicked() {
+    var menu = this.menu[0];
+    menu.classList.toggle('open');
+  }
 }
